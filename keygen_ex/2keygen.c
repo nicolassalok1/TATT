f3e40747d50659bc4a210c507aace364024ed8c9
Nicolas Pelletier <nicolas.pelletier@epita.fr>
2018-03-16 17:55:36 +0100

    RENDU FINIIIIII

--- keygen_ex/2keygen.c
+++ keygen_ex/2keygen.c
@@ -2,20 +2,21 @@
 
 int main ()
 {
-	int code = 0x90;
+	int code1 = 0x83;
+	int code2 = 0xc4;
+	int code3 = 0x04;
+	int code4 = 0xeb;
+	int code5 = 0x70;
+
 
 	int fd = fopen ("OG/crackme", "r+");
-
-	int fs = fseek(fd, 1565, SEEK_SET);
 	
-	fwrite(&code, 1, 1, fd);
-	fwrite(&code, 1, 1, fd);
-	
-	fs = fseek(fd, 1613, SEEK_SET);
+	int fs = fseek(fd, 1540, SEEK_SET);
 
-	fwrite(&code, 1, 1, fd);
-	fwrite(&code, 1, 1, fd);
-	fwrite(&code, 1, 1, fd);
-	fwrite(&code, 1, 1, fd);
+	fwrite(&code1, 1, 1, fd);
+	fwrite(&code2, 1, 1, fd);
+	fwrite(&code3, 1, 1, fd);
+	fwrite(&code4, 1, 1, fd);
+	fwrite(&code5, 1, 1, fd);
 	fclose (fd);
 }
